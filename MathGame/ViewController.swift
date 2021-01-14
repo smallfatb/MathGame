f73963417beaa26b8e2533d55e158d7a4da340ff
Angus Lai <angus.avea@gmail.com>
2021-01-14 13:43:55 -0800

    Initial Commit

---
+++ MathGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MathGame
+//
+//  Created by Angus on 14/1/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
